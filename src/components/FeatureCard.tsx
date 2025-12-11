2e26bfc03e45306076b649049c5adb614ec86fce
AbhiroopGhosh-007 <abhiroopghosh007@gmail.com>
2025-12-11 16:47:01 +0530

    Initial commit

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -8,7 +8,7 @@ interface FeatureCardProps {
 
 const FeatureCard: React.FC<FeatureCardProps> = ({ title, description, image }) => {
   return (
-    <article className="bg-card flex flex-col text-base items-center px-11 py-8 mobile375:px-16 mobile375:mx-1 rounded-2xl shadow-lg hover:shadow-xl transition-shadow h-full">
+    <article className="bg-card flex flex-col text-base items-center px-14 py-10 mobile375:px-16 mobile375:mx-1 rounded-2xl shadow-lg hover:shadow-xl transition-shadow h-full">
       <div className="w-full h-40 flex items-center justify-center mb-4">
         <img 
           src={image}
